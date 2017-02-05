634ad77efdbad5386c0e4f82166f19eb2e8894f0
Paul Hatcher <paulh@atrios.co.uk>
2017-02-05 11:06:15 +0000

    Update to latest logging version

--- code/SolutionInfo.cs
+++ code/SolutionInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("1.0.6")]
 [assembly: AssemblyFileVersionAttribute("1.0.6")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.6 3c64cd")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.6 a74634")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyVersion = "1.0.6";
         internal const System.String AssemblyFileVersion = "1.0.6";
-        internal const System.String AssemblyInformationalVersion = "1.0.6 3c64cd";
+        internal const System.String AssemblyInformationalVersion = "1.0.6 a74634";
     }
 }
